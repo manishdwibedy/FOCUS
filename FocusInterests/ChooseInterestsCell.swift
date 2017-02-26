c1abf127d2c6a717e9ba5f5feafdb4793a042327
jonathan thornburg <reliable2192@gmail.com>
2017-02-26 17:15:27 -0600

    fixes constraints in login view

---
+++ FocusInterests/ChooseInterestsCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChooseInterestsCell.swift
+//  FocusInterests
+//
+//  Created by jonathan thornburg on 2/25/17.
+//  Copyright © 2017 singlefocusinc. All rights reserved.
+//
+
+import UIKit
+
+class ChooseInterestsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
