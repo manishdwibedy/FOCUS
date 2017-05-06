1701401facd5ea64e78de03f95f43d2a75c6feea
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-05-06 01:42:12 -0700

    Starting with messages.

---
+++ FocusInterests/MessagesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MessagesViewController.swift
+//  FocusInterests
+//
+//  Created by Manish Dwibedy on 5/6/17.
+//  Copyright © 2017 singlefocusinc. All rights reserved.
+//
+
+import UIKit
+
+class MessagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func goBack(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
