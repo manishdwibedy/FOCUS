549435312b9b23e7a7d87ee85187360bcdcfffea
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-05-06 02:05:28 -0700

    Adding static chat message window.

---
+++ FocusInterests/ChatViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ChatViewController.swift
+//  FocusInterests
+//
+//  Created by Manish Dwibedy on 5/6/17.
+//  Copyright © 2017 singlefocusinc. All rights reserved.
+//
+
+import UIKit
+import JSQMessagesViewController
+
+class ChatViewController: JSQMessagesViewController {
+    let user1 = User(username: "Name 1", uuid: "1", userImage: nil, interests: nil)
+    let user2 = User(username: "Name 2", uuid: "2", userImage: nil, interests: nil)
+    
+    var messages = [JSQMessage]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // tell JSQMessagesViewController
+        // who is the current user
+        self.senderId = user1.uuid
+        self.senderDisplayName = user2.username
+        
+        
+        self.messages = getMessages()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        let message = JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text)
+        
+        messages.append(message!)
+        
+        finishSendingMessage()
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString! {
+        let message = messages[indexPath.row]
+        let messageUsername = message.senderDisplayName
+        
+        return NSAttributedString(string: messageUsername!)
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat {
+        return 15
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        
+        let message = messages[indexPath.row]
+        
+        if self.user1.uuid == message.senderId {
+            return bubbleFactory?.outgoingMessagesBubbleImage(with: .green)
+        } else {
+            return bubbleFactory?.incomingMessagesBubbleImage(with: .blue)
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.row]
+    }
+
+    func getMessages() -> [JSQMessage] {
+        var messages = [JSQMessage]()
+        
+        let message1 = JSQMessage(senderId: "1", displayName: "Steve", text: "Hey Tim how are you?")
+        let message2 = JSQMessage(senderId: "2", displayName: "Tim", text: "Fine thanks, and you?")
+        
+        messages.append(message1!)
+        messages.append(message2!)
+        
+        return messages
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
