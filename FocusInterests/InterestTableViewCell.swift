9b34e73eaf6645facc92b65ec565bb97ff345d91
Amber Spadafora <ahelenespadafora@gmail.com>
2017-05-09 10:45:35 -0700

    added customer interestCell

--- FocusInterests/InterestTableViewCell.swift
+++ FocusInterests/InterestTableViewCell.swift
@@ -10,17 +10,18 @@ import UIKit
 
 class InterestTableViewCell: UITableViewCell {
 
-    
-    @IBOutlet weak var selectedInterestLabel: UILabel!
     @IBOutlet weak var checkMarkImageView: UIImageView!
-    
+    @IBOutlet weak var selectedInterestLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
     }
 
 }
