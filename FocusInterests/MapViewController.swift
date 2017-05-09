039d4662c3b9813618a5cb126da21afc3b9ffa7f
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-05-08 19:23:14 -0700

    Now, the events can have non-string values as well.

--- FocusInterests/MapViewController.swift
+++ FocusInterests/MapViewController.swift
@@ -47,11 +47,9 @@ class MapViewController: BaseViewController, CLLocationManagerDelegate, GMSMapVi
             let events = snapshot.value as? [String : Any] ?? [:]
             
             for (id, event) in events{
-                let info = event as? [String:String]
-                let event = Event(title: (info?["title"])!, description: (info?["description"])!, fullAddress: (info?["fullAddress"])!, shortAddress: (info?["shortAddress"])!, latitude: (info?["latitude"])!, longitude: (info?["longitude"])!, date: (info?["date"])!, creator: (info?["creator"])!, id: id)
+                let info = event as? [String:Any]
+                let event = Event(title: (info?["title"])! as! String, description: (info?["description"])! as! String, fullAddress: (info?["fullAddress"])! as! String, shortAddress: (info?["shortAddress"])! as! String, latitude: (info?["latitude"])! as! String, longitude: (info?["longitude"])! as! String, date: (info?["date"])! as! String, creator: (info?["creator"])! as! String, id: id)
         
-                
-                
                 let position = CLLocationCoordinate2D(latitude: Double(event.latitude!)!, longitude: Double(event.longitude!)!)
                 let marker = GMSMarker(position: position)
                 marker.icon = UIImage(named: "addUser")
