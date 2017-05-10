5cad0e16ce364ce4a12ea35858985928bbc4db1f
Andrew Simpson <ajsimps@me.com>
2017-05-10 12:24:28 -0500

    added commentView.xib and matching UIView file

--- FocusInterests/commentView.swift
+++ FocusInterests/commentView.swift
@@ -2,7 +2,7 @@
 //  commentView.swift
 //  FocusInterests
 //
-//  Created by Andrew Simpson on 5/8/17.
+//  Created by Andrew Simpson on 5/10/17.
 //  Copyright © 2017 singlefocusinc. All rights reserved.
 //
 
@@ -10,12 +10,6 @@ import UIKit
 
 class commentView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    
 
 }
