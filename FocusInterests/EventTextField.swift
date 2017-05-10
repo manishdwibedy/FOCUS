0fa5c05c2c78f7bbf1b6dd84121010d81fd6fe29
Amber Spadafora <ahelenespadafora@gmail.com>
2017-05-10 01:55:13 -0700

    changed eventsStoryboard to CreateEvent.storyboard and uncommented the checkLogin func in appDelegate

---
+++ FocusInterests/EventTextField.swift
@@ -0,0 +1,15 @@
+//
+//  EventTextField.swift
+//  FocusInterests
+//
+//  Created by Amber Spadafora on 5/9/17.
+//  Copyright © 2017 singlefocusinc. All rights reserved.
+//
+
+import UIKit
+
+class EventTextField: UITextField {
+
+    
+
+}
