49591efae98895055e727fc804b1e728d03e2ccc
Amber Spadafora <ahelenespadafora@gmail.com>
2017-05-16 20:59:59 -0700

    made ui changes to createEvent, sendInvite VC

--- FocusInterests/InterestTableViewCell.swift
+++ FocusInterests/InterestTableViewCell.swift
@@ -10,20 +10,16 @@ import UIKit
 
 class InterestTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var checkMarkImageView: UIImageView!
     @IBOutlet weak var selectedInterestLabel: UILabel!
-    
-    
+    @IBOutlet weak var checkmarkBttn: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
+    
+    
 }
