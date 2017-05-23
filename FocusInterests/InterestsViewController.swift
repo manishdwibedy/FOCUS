d021f1e2843f8c0b5a3676f59fc07adfc45c5947
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-05-22 17:06:05 -0700

    Adding the new choose interest screen.

--- FocusInterests/InterestsViewController.swift
+++ FocusInterests/InterestsViewController.swift
@@ -9,56 +9,54 @@
 import UIKit
 import FacebookCore
 import FBSDKCoreKit
+import Magnetic
+import SpriteKit
 
-class InterestsViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate, UITableViewDelegate {
+class InterestsViewController: UIViewController{
 
-    @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var tableView: UITableView!
     
     var interests = [Interest]()
     let backgroundColor = UIColor.init(red: 22/255, green: 42/255, blue: 64/255, alpha: 1)
     var filtered = [Interest]()
     var searching = false
     
+    @IBOutlet weak var magneticView: MagneticView!{
+        didSet {
+            magnetic.magneticDelegate = self
+
+        }
+    }
+    
+    var magnetic: Magnetic {
+        return magneticView.magnetic
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        searchBar.delegate = self
+        self.magnetic.backgroundColor = UIColor.lightGray
+        for index in 1..<12 {
+            add(name: "Interest \(index)")
+        }
         
-        let interestCell = UINib(nibName: "InterestViewCell", bundle: nil)
-        tableView.register(interestCell, forCellReuseIdentifier: "cell")
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "label")
+    }
+    
+    func add(name: String) {
+        let name = name
+        let color = UIColor.green
+        let node = CustomNode(text: name.capitalized, image: UIImage(named: "addUser"), color: color, radius: 40)
+        node.strokeColor = UIColor.black
+        magnetic.addChild(node)
         
-        let backgroundView = UIView(frame: CGRect(x: 0, y: 0, width: self.tableView.bounds.size.width, height: self.tableView.bounds.size.height))
-        backgroundView.backgroundColor = self.backgroundColor
-        self.tableView.backgroundView = backgroundView
-        self.tableView.tableFooterView = UIView()
+        
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let textFieldInsideSearchBar = searchBar.value(forKey: "searchField") as? UITextField
-        textFieldInsideSearchBar?.backgroundColor = UIColor.darkGray
         
-        for view in searchBar.subviews.last!.subviews {
-            if view.isKind(of: NSClassFromString("UISearchBarBackground")!)
-            {
-                view.removeFromSuperview()
-            }
-        }
-        
-        for i in 1...10{
-            let interest = Interest(name: "Interest \(i)", category: nil, image: nil, imageString: nil)
-            interests.append(interest)
-            filtered.append(interest)
-        }
-        
-        tableView.dataSource = self
-        tableView.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -70,87 +68,6 @@ class InterestsViewController: UIViewController, UITableViewDataSource, UISearch
         self.dismiss(animated: true, completion: nil)
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if self.searching{
-            return self.filtered.count
-        }
-        return self.filtered.count + 3
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if !searching && indexPath.row == 0{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! InterestViewCell
-            cell.interestName.text = "All My interests"
-            cell.interestName.font = .boldSystemFont(ofSize: 16.0)
-            cell.interestName.textColor = UIColor.white
-            cell.backgroundColor = self.backgroundColor
-            return cell
-        }
-        else if !searching && indexPath.row == 1{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "label", for: indexPath)
-            cell.textLabel?.text = "Clear All"
-            cell.textLabel?.font = UIFont.systemFont(ofSize: 12)
-            cell.textLabel?.textColor = UIColor.white
-            cell.backgroundColor = self.backgroundColor
-            return cell
-        }
-        else if !searching && indexPath.row == 2{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "label", for: indexPath)
-            cell.textLabel?.text = "Top"
-            cell.textLabel?.font = UIFont.systemFont(ofSize: 12)
-            cell.textLabel?.textColor = UIColor.green
-            cell.backgroundColor = self.backgroundColor
-            return cell
-        }
-        else{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! InterestViewCell
-            if searching{
-                cell.interestName.text = self.filtered[indexPath.row].name
-            }
-            else{
-                cell.interestName.text = self.filtered[indexPath.row - 3].name
-            }
-            
-            cell.textLabel?.font = UIFont.systemFont(ofSize: 16)
-            cell.interestName.textColor = UIColor.white
-            cell.backgroundColor = self.backgroundColor
-            return cell
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-    }
-
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        self.searching = true
-    }
-    
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        self.searching = false;
-    }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        self.searching = false;
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        self.searching = false;
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText.characters.count > 0{
-            self.filtered = self.interests.filter { ($0.name?.contains(searchText))! }
-            self.searching = true
-        }
-        else{
-            self.searching = false;
-            self.filtered = self.interests
-        }
-        
-        self.tableView.reloadData()
-    }
     
     /*
     // MARK: - Navigation
@@ -162,4 +79,180 @@ class InterestsViewController: UIViewController, UITableViewDataSource, UISearch
     }
     */
 
+}
+
+// MARK: - MagneticDelegate
+extension InterestsViewController: MagneticDelegate {
+    
+    func magnetic(_ magnetic: Magnetic, didSelect node: Node) {
+        print("didSelect -> \(node)")
+    }
+    
+    func magnetic(_ magnetic: Magnetic, didDeselect node: Node) {
+        print("didDeselect -> \(node)")
+    }
+    
+}
+
+// MARK: - ImageNode
+class ImageNode: Node {
+    override var image: UIImage? {
+        didSet {
+            sprite.texture = image.map { SKTexture(image: $0) }
+        }
+    }
+    override func selectedAnimation() {}
+    override func deselectedAnimation() {}
+}
+
+extension Magnetic{
+    
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("begin touching")
+        self.touchBegan = Date()
+    }
+    
+    override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // if can get last tap time
+        if let lastTap = self.lastTapTime{
+            let interval = Date().timeIntervalSince(lastTap)
+            if Double(interval) < 0.2{
+                print("double tap")
+                print(interval)
+                self.isDoubleTap = true
+            }
+            else{
+                self.isDoubleTap = false
+            }
+        }
+        
+        // check for long hold
+        
+        let tapduration = Date().timeIntervalSince(self.touchBegan!)
+        
+        if Double(tapduration) > 0.5{
+            print("long hold")
+            self.isLongTap = true
+        }
+        else{
+            self.isLongTap = false
+            self.touchBegan = Date()
+        }
+        self.lastTapTime = Date()
+        self.touchBegan = nil
+        if let point = touches.first?.location(in: self), let node = atPoint(point) as? CustomNode {
+            
+            if isDoubleTap{
+                node.setDoubleTap()
+            }
+            else{
+                node.resetDoubleTap()
+            }
+            
+            if isLongTap{
+                node.setLongTap()
+            }
+            else{
+                node.resetLongTap()
+            }
+            
+            if node.isSelected {
+                node.state = .normal
+                magneticDelegate?.magnetic(self, didDeselect: node)
+            } else {
+                if !allowsMultipleSelection, let selectedNode = selectedChildren.first as? CustomNode {
+                    selectedNode.state = .like
+                    magneticDelegate?.magnetic(self, didDeselect: selectedNode)
+                }
+                
+                node.state = .like
+            }
+        }
+    }
+
+}
+
+enum State{
+    case normal
+    case like
+    case love
+    case hate
+}
+
+class CustomNode: Node{
+    var isDoubleTap = false
+    var isLongTap = false
+    
+    func setDoubleTap(){
+        self.isDoubleTap = true
+    }
+    
+    func resetDoubleTap(){
+        self.isDoubleTap = false
+    }
+    
+    func setLongTap(){
+        self.isLongTap = true
+    }
+    
+    func resetLongTap(){
+        self.isLongTap = false
+    }
+    
+    override var image: UIImage? {
+        didSet {
+            texture = image.map { SKTexture(image: $0) }
+        }
+    }
+    
+    var texture: SKTexture?
+    
+    var state: State = .normal {
+        didSet {
+            //guard state != oldValue else { return }
+            switch(state){
+                case .normal: fallthrough
+                case .like: fallthrough
+                case .hate: fallthrough
+                case .love:
+                    let selectionTask = DispatchWorkItem {
+                        print(" show tap - \(Date().timeIntervalSince1970) \(self.isDoubleTap)")
+                        if self.isLongTap{
+                            self.hateInterest()
+                        }
+                        else if !self.isDoubleTap{
+                            self.likeInterest()
+                        }
+                        else{
+                            self.loveInterest()
+                        }
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.2, execute: selectionTask)
+                }
+            
+            
+            }
+    
+    }
+    
+    func hateInterest(){
+        if let texture = self.texture {
+            self.sprite.run(.setTexture(texture))
+        }
+        self.run(.scale(to: 0.5, duration: 0.2))
+    }
+    
+    func likeInterest(){
+        if let texture = self.texture {
+            sprite.run(.setTexture(texture))
+        }
+        run(.scale(to: 1.5, duration: 0.2))
+    }
+    
+    func loveInterest(){
+        if let texture = self.texture {
+            sprite.run(.setTexture(texture))
+        }
+        run(.scale(to: 2, duration: 0.2))
+    }
 }
