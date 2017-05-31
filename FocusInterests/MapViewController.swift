2eca4aefe981b13e60139d21d379ff4fa812d6ea
Manish Dwibedy <manish.dwibdy@gmail.com>
2017-05-31 16:17:21 -0700

    testing.

--- FocusInterests/MapViewController.swift
+++ FocusInterests/MapViewController.swift
@@ -380,6 +380,7 @@ class MapViewController: BaseViewController, CLLocationManagerDelegate, GMSMapVi
             })
         }
         
+        
         print("got location")
         if let token = AuthApi.getYelpToken(){
             self.fetchPlaces(around: self.currentLocation!, token: token)
