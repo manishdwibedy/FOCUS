d4dc344d87bfb2939ede1e3ec4a63c50e701f073
theorangajang <alexjangspammyspam@gmail.com>
2017-06-07 01:47:16 -0700

    created Comments storyboard, CommentsTableViewCell with its xib file, and CommentsViewController

--- FocusInterests/MapViewController.swift
+++ FocusInterests/MapViewController.swift
@@ -585,6 +585,7 @@ class MapViewController: BaseViewController, CLLocationManagerDelegate, GMSMapVi
         let VC:UIViewController = UIStoryboard(name: "Messages", bundle: nil).instantiateViewController(withIdentifier: "Home") as! UINavigationController
         
         self.present(VC, animated:true, completion:nil)
+
     }
     
     func notificationsClicked() {
