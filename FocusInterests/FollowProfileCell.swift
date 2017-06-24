1a9c550d46f63e77c2d3d9ec610061ca6cae0094
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-06-24 05:37:01 -0700

    Fixed event detail and guest list.

--- FocusInterests/FollowProfileCell.swift
+++ FocusInterests/FollowProfileCell.swift
@@ -44,22 +44,12 @@ class FollowProfileCell: UITableViewCell {
         print(data.uid)
         ref.child("users").child(data.uid).observeSingleEvent(of: .value, with: { (snapshot) in
             let value = snapshot.value as? NSDictionary
-            if value != nil
+            if let value = value
             {
-                if let fullNameVal = value?["fullname"] as? String{
-                    if (fullNameVal == ""){
-                        self.usernameLabel.text = value?["username"] as? String
-                    }else{
-                        self.fullnameLabel.text = fullNameVal
-                        if let usernameLabel = self.usernameLabel.text{
-                            //var usernameAttribute = NSMutableAttributedString()
-                            //usernameAttribute = NSMutableAttributedString(string: usernameLabel, attributes: [NSFontAttributeName:UIFont(name: "Avenir Black", size: 15.0)!])
-                            self.usernameLabel.text = usernameLabel
-                        }
-                    }
-                }
+                self.fullnameLabel.text = value["fullname"] as? String
+                self.usernameLabel.text = value["username"] as? String
                 
-                self.data.username = (value?["username"] as? String)!
+                self.data.username = (value["username"] as? String)!
                 self.ifFollowing(uid: self.data.uid, completionIt: {(boolV) -> () in
                 
                     if boolV == true
