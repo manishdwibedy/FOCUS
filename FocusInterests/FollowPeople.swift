d71da4d2ad99333c03c6a4bff30ac3ffbf5f045d
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-06-29 19:33:04 -0700

    following only once.

--- FocusInterests/FollowPeople.swift
+++ FocusInterests/FollowPeople.swift
@@ -10,10 +10,24 @@ import Foundation
 
 class Follow{
     static func followUser(uid: String){
-        Constants.DB.user.child(AuthApi.getFirebaseUid()!).child("following/people").childByAutoId().updateChildValues(["UID": uid])
         
-        Constants.DB.user.child(uid).child("followers/people").childByAutoId().updateChildValues(["UID": AuthApi.getFirebaseUid()!])
+        Constants.DB.user.child(AuthApi.getFirebaseUid()!).child("following/people").queryOrdered(byChild: "UID").queryEqual(toValue: uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            let value = snapshot.value as? [String:Any]
+            
+            if value == nil{
+                Constants.DB.user.child(AuthApi.getFirebaseUid()!).child("following/people").childByAutoId().updateChildValues(["UID": uid])
+                
+            }
+        })
         
+        Constants.DB.user.child(uid).child("followers").child("people").queryOrdered(byChild: "UID").queryEqual(toValue: AuthApi.getFirebaseUid()!).observeSingleEvent(of: .value, with: { (snapshot) in
+            let value = snapshot.value as? [String:Any]
+            
+            if value == nil{
+                Constants.DB.user.child(uid).child("followers/people").childByAutoId().updateChildValues(["UID": AuthApi.getFirebaseUid()!])
+                
+            }
+        })
     }
     
     static func unFollowUser(uid: String){
