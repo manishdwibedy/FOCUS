011e01cb535e75e53a0dfdcc30a42ee669a7cf95
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-06-30 19:32:01 -0700

    fixed the same in search as well.

--- FocusInterests/SearchPeopleViewController.swift
+++ FocusInterests/SearchPeopleViewController.swift
@@ -271,28 +271,32 @@ class SearchPeopleViewController: UIViewController, UITableViewDelegate,UITableV
                 for (_, user) in users{
                     let info = user as? [String:Any]
                     
-                    
-                    let user = User(username: info?["username"] as! String?, fullname: info?["fullname"] as! String? , uuid: info?["firebaseUserId"] as! String?, userImage: nil, interests: nil, image_string: nil, hasPin: false)
-                    
-                    if user.uuid != AuthApi.getFirebaseUid(){
-                        Constants.DB.pins.child(user.uuid!).observeSingleEvent(of: .value, with: { (snapshot) in
-                            let value = snapshot.value as? NSDictionary
-                            if value != nil
-                            {
-                                var address = value?["formattedAddress"] as! String
-                                address = address.replacingOccurrences(of: ";;", with: "\n")
-                                let data = pinData(UID: value?["fromUID"] as! String, dateTS: (value?["time"] as! Double), pin: (value?["pin"] as! String), location: (value?["formattedAddress"] as! String), lat: (value?["lat"] as! Double), lng: (value?["lng"] as! Double), path: Constants.DB.pins.child(user.uuid! as! String), focus: value?["focus"] as! String)
-                                user.hasPin = true
-                                self.user_pins[user.uuid!] = data
-                            }
-                            self.filtered.append(user)
-                            
-                            if self.filtered.count == count{
+                    if let user = User.toUser(info: info){
+                        if user.uuid != AuthApi.getFirebaseUid(){
+                            Constants.DB.pins.child(user.uuid!).observeSingleEvent(of: .value, with: { (snapshot) in
+                                let value = snapshot.value as? NSDictionary
+                                if let value = value
+                                {
+                                    if let pin = pinData.toPin(user: user, value: value){
+                                        self.user_pins[user.uuid!] = pin
+                                        user.hasPin = true
+                                        
+                                        let pinLocation = CLLocation(latitude: pin.coordinates.latitude, longitude: pin.coordinates.longitude)
+                                        user.pinDistance = pinLocation.distance(from: AuthApi.getLocation()!)
+                                    }
+                                }
+                                self.filtered.append(user)
+                                
+                                self.filtered.sort {
+                                    if $0.hasPin && $1.hasPin{
+                                        return $0.pinDistance < $1.pinDistance
+                                    }
+                                    return $0.hasPin && !$1.hasPin
+                                }
+                                
                                 self.tableView.reloadData()
-                            }
-                        })
-                        
-                        
+                            })
+                        }
                     }
                 }
                 
