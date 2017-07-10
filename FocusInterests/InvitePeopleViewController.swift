282644d1e02dc574f3b294e87d5573f1dc990b4c
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-07-09 23:10:59 -0700

    showing meet up event interest.

--- FocusInterests/InvitePeopleViewController.swift
+++ FocusInterests/InvitePeopleViewController.swift
@@ -357,13 +357,13 @@ class InvitePeopleViewController: UIViewController,UITableViewDelegate,UITableVi
         Constants.DB.event.observeSingleEvent(of: .value, with: { (snapshot) in
             let value = snapshot.value as? NSDictionary
             self.filtered.removeAll()
-            if value != nil
+            if let value = value
             {
-                for (id, event) in value!
+                for (id, event) in value
                 {
                     if let info = event as? [String:Any]{
                         let event = Event.toEvent(info: info)
-                        //                    let event = Event(title: (info?["title"])! as! String, description: (info?["description"])! as! String, fullAddress: (info?["fullAddress"])! as? String, shortAddress: (info?["shortAddress"])! as! String, latitude: (info?["latitude"])! as! String, longitude: (info?["longitude"])! as! String, date: (info?["date"])! as! String, creator: (info?["creator"])! as! String, id: id as? String, category: info?["interest"] as? String, privateEvent: (info?["private"] as? Bool)!)
+                        event?.id = id as? String
                         self.filtered.append(event)
                     }
                     
