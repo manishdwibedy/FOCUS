83a0b9a0b8b55415d4373ad4ff686a74d050084b
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-07-13 13:26:05 -0700

    pin username and interests now have same font.

--- FocusInterests/GlobalFunctions.swift
+++ FocusInterests/GlobalFunctions.swift
@@ -680,11 +680,11 @@ func attributedString(from string: String, nonBoldRange: NSRange?) -> NSAttribut
 
 func attributedString(from string: String, boldRange: NSRange?) -> NSAttributedString {
     let attrs = [
-        NSFontAttributeName: UIFont(name: "Avenir-Black", size: 15),
+        NSFontAttributeName: UIFont(name: "Avenir-Black", size: 17),
         NSForegroundColorAttributeName: UIColor.white
     ]
     let nonBoldAttribute = [
-        NSFontAttributeName: UIFont(name: "Avenir-Book", size: 15),
+        NSFontAttributeName: UIFont(name: "Avenir-Book", size: 17),
         NSForegroundColorAttributeName: UIColor.white
     ]
     let attrStr = NSMutableAttributedString(string: string, attributes: nonBoldAttribute)
