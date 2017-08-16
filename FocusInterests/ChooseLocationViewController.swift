6ccaed6ba3410b0aecb249db2bdb14e57d53e2b2
theorangajang <alexjangspammyspam@gmail.com>
2017-08-15 18:04:07 -0700

    changed background color of choose location cell

--- FocusInterests/ChooseLocationViewController.swift
+++ FocusInterests/ChooseLocationViewController.swift
@@ -173,15 +173,6 @@ class ChooseLocationViewController: UIViewController, UITableViewDelegate, UITab
         searchBar.resignFirstResponder()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func backPressed(_ sender: Any) {
         if let loc = self.selectedLocation{
             delegate.gotSelectedLocation(location: self.selectedLocation!)
